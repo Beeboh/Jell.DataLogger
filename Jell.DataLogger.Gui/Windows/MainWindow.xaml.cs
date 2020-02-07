ace02ad43d42e179baa942fe69bb8bd9f553d2e7
Eric Graham <egraham@ualberta.ca>
2020-02-06 23:39:09 -0700

    Added menu and Record New Data window

--- Jell.DataLogger.Gui/Windows/MainWindow.xaml.cs
+++ Jell.DataLogger.Gui/Windows/MainWindow.xaml.cs
@@ -19,9 +19,79 @@ namespace Jell.DataLogger.Gui.Windows
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool _connectedmode;
+        private bool ConnectedMode
+        {
+            get { return _connectedmode; }
+            set
+            {
+                foreach (MenuItem connectedmenuitem in ConnectedMenuItems)
+                {
+                    connectedmenuitem.IsEnabled = value;
+                }
+                foreach(MenuItem disconnectedmenuitem in DisconnectedMenuItems)
+                {
+                    disconnectedmenuitem.IsEnabled = !value;
+                }
+                _connectedmode = value;
+            }
+        }
+        private MenuItem[] ConnectedMenuItems { get; }
+        private MenuItem[] DisconnectedMenuItems { get; }
+        //{
+        //    get { return _connectedmode; }
+        //    set
+        //    {
+        //        _connectedmode = value;
+        //        OnPropertyChanged("ConnectedMode");
+        //    }
+        //}
+
         public MainWindow()
         {
             InitializeComponent();
+            ConnectedMenuItems = GetConnectedMenuItems();
+            DisconnectedMenuItems = GetDisconnectedMenuItems();
+            Disconnect();
+        }
+        private void Connect()
+        {
+            ConnectedMode = true;
+
+        }
+        private void Disconnect()
+        {
+            ConnectedMode = false;
+        }
+        private MenuItem[] GetConnectedMenuItems()
+        {
+            return new MenuItem[] { menuDisconnect, menuRecordNewData, menuExportToExcel };
+        }
+        private MenuItem[] GetDisconnectedMenuItems()
+        {
+            return new MenuItem[] { menuConnect };
+        }
+
+
+        //Buttons
+        private void menuConnect_Click(object sender, RoutedEventArgs e)
+        {
+            Connect();
+            
+        }
+        private void menuDisconnect_Click(object sender, RoutedEventArgs e)
+        {
+            ConnectedMode = false;
+        }
+        private void menuExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void menuRecordNewData_Click(object sender, RoutedEventArgs e)
+        {
+            RecordNewData RecordDataWindow = new RecordNewData();
+            RecordDataWindow.ShowDialog();
         }
     }
 }
