328d54a6a14e71db852cf28fa8e09d4fdbc2ce5f
Eric Graham <egraham@ualberta.ca>
2020-02-10 08:19:37 -0700

    Updated RecordNewData and started on the tabular data view

---
+++ Jell.DataLogger.Gui/Views/ConnectedScreenView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Jell.DataLogger.Gui.Views
+{
+    /// <summary>
+    /// Interaction logic for ConnectedScreen.xaml
+    /// </summary>
+    public partial class ConnectedScreen : UserControl
+    {
+        public ConnectedScreen()
+        {
+            InitializeComponent();
+        }
+    }
+}
